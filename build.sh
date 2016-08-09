58e829826f5d8c24615f2550ddab77fb0e8d4420
rafathefull <rafa.thefull@gmail.com>
2016-08-09 12:55:46 +0200

    Fix. Name bad in build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-HARBOUR_HOME="$HOME/build/harbour-git"
-HARBOUR_PLAT="darwin/gcc"
-HBMK2="$HARBOUR_HOME/bin/$HARBOUR_PLAT/hbmk2 -q0 -w2 -es2 -debug -optim- -cflag=-Wno-missing-field-initializers"
+HARBOUR_HOME="$HOME/harbour-core"
+HARBOUR_PLAT="linux/gcc"
+HBMK2="$HARBOUR_HOME/bin/$HARBOUR_PLAT/hbmk2 -q0 -w1 -es2 -debug -optim- -cflag=-Wno-missing-field-initializers -trace"
 
 # If HBGI_DYN is set to "no", build everything statically; helps debugging
 HBGI_DYN="no"
@@ -31,33 +31,34 @@ HBGI_SOURCES="
         hbgi-type.c
         "
 
-HBGOBJECT_FLAGS="$(pkg-config --cflags --libs glib-2.0)"
+HBGOBJECT_FLAGS="$(pkg-config --cflags --libs glib-2.0)" 
+#SOURCE_FLAGS="$(pkg-config --cflags --libs gtksourceview-3.0)"
 HBGI_FLAGS="$(pkg-config --cflags --libs gobject-introspection-1.0)"
 
 for F in $HBGIHB_SOURCES; do HBGIHB_SOURCES_PREFIXED="$HBGIHB_SOURCES_PREFIXED hb/$F"; done
 for F in $HBGOBJECT_SOURCES; do HBGOBJECT_SOURCES_PREFIXED="$HBGOBJECT_SOURCES_PREFIXED gobject/$F"; done
 for F in $HBGI_SOURCES; do HBGI_SOURCES_PREFIXED="$HBGI_SOURCES_PREFIXED gi/$F"; done
 
-if [ $HBGI_DYN == "no" ]; then
+#if [ $HBGI_DYN == "no" ]; then
 
 cd hb
-$HBMK2 -hblib -o../hbgihb $HBGIHB_SOURCES || exit $?
+$HBMK2 -hblib -o../hbgihb $HBGIHB_SOURCES  || exit $?
 
 cd ../gobject
-$HBMK2 -hblib -o../hbgobject -I../hb -I../gi $HBGOBJECT_FLAGS $HBGOBJECT_SOURCES || exit $?
+$HBMK2 -hblib -o../hbgobject -I../hb -I../gi $HBGOBJECT_FLAGS $SOURCE_FLAGS $HBGOBJECT_SOURCES || exit $?
 
 cd ../gi
 $HBMK2 -hblib -o../hbgi -I../hb -I../gobject -I../gi $HBGI_FLAGS $HBGI_SOURCES || exit $?
 
 cd ..
 
-$HBMK2 -L. -lhbgihb -lhbgobject -lhbgi -gtcgi $HBGI_FLAGS test.prg || exit $?
+$HBMK2 -L. -lhbgihb -lhbgobject -lhbgi -gtcgi $HBGI_FLAGS $SOURCE_FLAGS test.prg || exit $?
 
-else
+#else
 
-$HBMK2 -hbdyn -ohbgi -g $HBGIHB_SOURCES_PREFIXED $HBGOBJECT_SOURCES_PREFIXED $HBGI_SOURCES_PREFIXED -Ihb -Igobject -Igi $HBGOBJECT_FLAGS $HBGI_FLAGS || exit $?
+#$HBMK2 -hbdyn -ohbgi -g $HBGIHB_SOURCES_PREFIXED $HBGOBJECT_SOURCES_PREFIXED $HBGI_SOURCES_PREFIXED -Ihb -Igobject -Igi $HBGOBJECT_FLAGS $HBGI_FLAGS || exit $?
 
 #$HBMK2 -L. -lhbgi -gtcgi test.prg || exit $?
-$HBMK2 -L. -lhbgi -L$HARBOUR_HOME/lib/$HARBOUR_PLAT -gtcgi test.prg || exit $?
+#$HBMK2 -L. -lhbgi -L$HARBOUR_HOME/lib/$HARBOUR_PLAT -gtcgi test.prg || exit $?
 
-fi
+#fi
