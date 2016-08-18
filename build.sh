7ca6dee2cdd876411281d97e3c089b4b45abd64f
Viktor Szakats <vszakats@users.noreply.github.com>
2016-08-18 17:38:06 +0200

    cleanup hbmk2 build system, add support for Windows MSYS2

--- build.sh
+++ build.sh
@@ -1,63 +1,16 @@
-#!/bin/sh
+#!/bin/sh -e
 
-HARBOUR_HOME="$HOME/build/harbour-git"
-HARBOUR_PLAT="darwin/gcc"
-HBMK2="$HARBOUR_HOME/bin/$HARBOUR_PLAT/hbmk2 -q0 -w2 -es2 -debug -optim- -cflag=-Wno-missing-field-initializers"
+# hbmk2 must be in PATH
 
-# If HBGI_DYN is set to "no", build everything statically; helps debugging
-HBGI_DYN="no"
+# Windows - MSYS2 packages required:
+#    pacman -S mingw-w64-{i686,x86_64}-{gtk2,gobject-introspection}
 
-HBGIHB_SOURCES="
-        hbgihb.c
-        "
+# static
+hbmk2 gihb.hbp
+hbmk2 gobject.hbp
+hbmk2 gi.hbp
+hbmk2 test.prg -otest_s -gtcgi hbgi.hbc
 
-HBGOBJECT_SOURCES="
-        hbgobject.c
-        hbginterface.c
-        hbgpointer.c
-        hbgtype.c
-        "
-
-HBGI_SOURCES="
-        hbgicore.c
-        hbgi-argument.c
-        hbgi-boxed.c
-        hbgi-callbacks.c
-        hbgi-closure.c
-        hbgi-info.c
-        hbgi-invoke.c
-        hbgi-signal-closure.c
-        hbgi-struct-marshal.c
-        hbgi-type.c
-        "
-
-HBGOBJECT_FLAGS="$(pkg-config --cflags --libs glib-2.0)"
-HBGI_FLAGS="$(pkg-config --cflags --libs gobject-introspection-1.0)"
-
-for F in $HBGIHB_SOURCES; do HBGIHB_SOURCES_PREFIXED="$HBGIHB_SOURCES_PREFIXED hb/$F"; done
-for F in $HBGOBJECT_SOURCES; do HBGOBJECT_SOURCES_PREFIXED="$HBGOBJECT_SOURCES_PREFIXED gobject/$F"; done
-for F in $HBGI_SOURCES; do HBGI_SOURCES_PREFIXED="$HBGI_SOURCES_PREFIXED gi/$F"; done
-
-if [ $HBGI_DYN == "no" ]; then
-
-cd hb
-$HBMK2 -hblib -o../hbgihb $HBGIHB_SOURCES || exit $?
-
-cd ../gobject
-$HBMK2 -hblib -o../hbgobject -I../hb -I../gi $HBGOBJECT_FLAGS $HBGOBJECT_SOURCES || exit $?
-
-cd ../gi
-$HBMK2 -hblib -o../hbgi -I../hb -I../gobject -I../gi $HBGI_FLAGS $HBGI_SOURCES || exit $?
-
-cd ..
-
-$HBMK2 -L. -lhbgihb -lhbgobject -lhbgi -gtcgi $HBGI_FLAGS test.prg || exit $?
-
-else
-
-$HBMK2 -hbdyn -ohbgi -g $HBGIHB_SOURCES_PREFIXED $HBGOBJECT_SOURCES_PREFIXED $HBGI_SOURCES_PREFIXED -Ihb -Igobject -Igi $HBGOBJECT_FLAGS $HBGI_FLAGS || exit $?
-
-#$HBMK2 -L. -lhbgi -gtcgi test.prg || exit $?
-$HBMK2 -L. -lhbgi -L$HARBOUR_HOME/lib/$HARBOUR_PLAT -gtcgi test.prg || exit $?
-
-fi
+# dynamic
+hbmk2 -hbdyn -shared '{win}-lhbmaindllp' @gi.hbp @gobject.hbp @gihb.hbp -ohbgidyn
+hbmk2 test.prg -otest_d -gtcgi -env:HBGI_DYNAMIC=yes hbgi.hbc
