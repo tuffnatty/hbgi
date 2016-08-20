febcaa22dd00cfb35c5079a597ed54b10638ac50
Viktor Szakats <vszakats@users.noreply.github.com>
2016-08-20 12:10:36 +0200

    build.sh: check requirements and show errors/instructions

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
 #!/bin/sh -e
 
-# hbmk2 must be in PATH
+if ! which hbmk2 > /dev/null; then
+   echo '! Error: hbmk2 missing from PATH'
+   exit 1
+fi
 
-# Windows - MSYS2 packages required:
-#    pacman -S mingw-w64-{i686,x86_64}-{gtk2,gobject-introspection}
+if ! pkg-config glib-2.0 || \
+   ! pkg-config gobject-introspection-1.0; then
+   echo '! Error: Required components missing: glib, gobject-introspection'
+   case "$(uname)" in
+      *_NT*)
+      echo '! Install using:'
+      echo '  pacman -S mingw-w64-{i686,x86_64}-{gtk2,glib,gobject-introspection}'
+      ;;
+   esac
+   exit 1
+fi
 
 # static
 hbmk2 gihb.hbp
